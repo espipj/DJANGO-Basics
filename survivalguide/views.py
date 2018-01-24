a9131f630bf02b60f30c05d8999c6b8b91f1e8d9
Pablo Espinosa <espipj@gmail.com>
2018-01-24 19:09:35 +0100

    added some views

--- survivalguide/views.py
+++ survivalguide/views.py
@@ -1,5 +1,14 @@
+from __future__ import absolute_import
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from django.views import generic
 
+from .forms import RegistrationForm
 
 class HomePageView(generic.TemplateView):
     template_name = 'home.html'
+
+class SignUpView(generic.CreateView):
+    form_class = RegistrationForm
+    model = UserCreationForm
+    template_name = 'accounts/signup.html'
